f396626ceac1798438c27678d1158feb17fcd5bb
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2025-01-26 10:25:47 +0900

    新しいインスタンスの作成

--- classes/BankAccount.cs
+++ classes/BankAccount.cs
@@ -16,17 +16,42 @@ namespace Classes;
 {}の内側は全てクラス状態と動作を定義している。
 このクラスには、5つのメンバがある。
 最初の3つはプロパティという。
-プロパティというのは、データを格納するための要素（メンバ）。
+プロパティというのは、クラスのデータ（属性）にアクセスするための仕組みのこと。
+オブジェクトがもっているデータのことを属性という。
+BankAccountクラスの属性は、Number(口座番号)、Owner(所有者名)、Balance(残高)の3つ。
+プロパティは、クラスの属性（データ）へのアクセスを安全に管理するための仕組みのこと。
 残りの2つはメソッドという。
-メソッドは1つの機能を実行するコードブロックのこと。
+メソッドは1つの機能を実行するコードブロックのこと。または、オブジェクトの振る舞いを定義するもの
 各メンバを確認すると、開発者がそのクラスの作用を把握するための十分な情報が得られるような構成・名前にする。
+*/
+
+/*
+BankAccount 型の新しいオブジェクトを作成する＝BankAccount 型のクラスのメンバに初期値を設定して、個々の口座を作成すること。
+田中さんの口座（残高1000円） <- 「田中さんの口座」オブジェクトは、BankAccount 型のクラスの性質を持っている。
+山本さんの口座（残高3500円） <- 「山本さんの口座」オブジェクトは、BankAccount 型のクラスの性質を持っている。
+2つの口座は、BankAccount 型のクラスの性質を持っているけど、それぞれ別のオブジェクトである。
 */
 public class BankAccount
 {
+	
+	private static int s_accountNumberSeed = 1234567890;
     public string Number { get; }
     public string Owner { get; set; }
     public decimal Balance { get; }
 
+	/*
+	コンストラクタ：BankAccountオブジェクト作成時に呼び出され、Owner(所有者名)とBalance(初期残高)を初期化する
+	初期化とは、オブジェクトのメンバに値を設定すること。
+	コンストラクタは、オブジェクトの作成時に呼び出される。
+	新しい山田さんのオブジェクトを作成するときに、コンストラクタを呼び出して、そのオブジェクトには名前と残高が存在することを保証する。
+	コンストラクタの名前は、クラス名と同じである。
+	コンストラクタに引数を渡すことで、オブジェクトのメンバに値を設定することができる
+	*/
+	public BankAccount(string name, decimal initialBalance)
+	{
+		this.Owner = name;
+		this.Balance = initialBalance;
+	}
     // 預金をするためのクラス
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
--- classes/Program.cs
+++ classes/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Classes;
+
+/*
+newをするとオブジェクトの作成をすることができる
+出力結果：Account  was created for yamasaka with 1000 initial balance.
+ただ、今の状態だと口座番号がない。
+なのでオブジェクトが作成される時に口座番号を設定する必要がある。
+だけど、これは呼び出し元（つまりnewしたこのプログラム）に負わせる責任ではない。
+*/
+var account = new BankAccount("yamasaka", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
